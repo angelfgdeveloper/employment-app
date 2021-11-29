d4c778639d07a39a1ba618d1f14f2d91f873452c
Luis Angel <angelfgdeveloper@gmail.com>
2021-11-28 19:10:18 -0600

    Añadiendo vista a informacion extra

--- app/src/main/java/com/angelfgdeveloper/manresapp/ui/question/ExtrasFragment.java
+++ app/src/main/java/com/angelfgdeveloper/manresapp/ui/question/ExtrasFragment.java
@@ -11,6 +11,8 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.angelfgdeveloper.manresapp.R;
 import com.angelfgdeveloper.manresapp.databinding.FragmentExtrasBinding;
@@ -21,6 +23,8 @@ public class ExtrasFragment extends Fragment {
     private FragmentExtrasBinding binding;
 
     private OnExtraFragmentListener listener;
+
+    private String mSelection = "", mContractJob = "", mReasonContract = "";
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -33,10 +37,11 @@ public class ExtrasFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mViewModel = new ViewModelProvider(this).get(QuestionViewModel.class);
         binding = FragmentExtrasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        addSelectData();
 
         binding.buttonBack.setOnClickListener(v -> Navigation.findNavController(root).popBackStack());
 
@@ -44,8 +49,31 @@ public class ExtrasFragment extends Fragment {
             listener.setQuestionInfo("Encuesta realizada!!!");
         });
 
-
         return root;
+    }
+
+    private void addSelectData() {
+        mViewModel.getAllYesAndNot().observe(getViewLifecycleOwner(), yesAndNot -> {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.dropdown_menu_item, yesAndNot);
+            binding.autoCompleteTextViewSelection.setAdapter(adapter);
+            binding.autoCompleteTextViewContractJobs.setAdapter(adapter);
+            binding.autoCompleteTextViewReasonContract.setAdapter(adapter);
+
+            binding.autoCompleteTextViewSelection.setOnItemClickListener((parent, view1, position, id) -> {
+                mSelection = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), mSelection + " - Posicion: " + position + " id: " + id, Toast.LENGTH_SHORT).show();
+            });
+
+            binding.autoCompleteTextViewContractJobs.setOnItemClickListener((parent, view1, position, id) -> {
+                mContractJob = parent.getItemAtPosition(position).toString();
+
+            });
+
+            binding.autoCompleteTextViewReasonContract.setOnItemClickListener((parent, view1, position, id) -> {
+                mReasonContract = parent.getItemAtPosition(position).toString();
+            });
+
+        });
     }
 
     public interface OnExtraFragmentListener {
