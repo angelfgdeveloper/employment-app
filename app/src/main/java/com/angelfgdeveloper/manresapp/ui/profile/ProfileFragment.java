617bb0886b04e003052be0ba21da1db250e970c5
Luis Angel <angelfgdeveloper@gmail.com>
2021-12-19 20:09:34 -0600

    Añadiendo postulaciones al perfil del usuario

--- app/src/main/java/com/angelfgdeveloper/manresapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/angelfgdeveloper/manresapp/ui/profile/ProfileFragment.java
@@ -13,8 +13,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.angelfgdeveloper.manresapp.R;
+import com.angelfgdeveloper.manresapp.data.models.Postulation;
 import com.angelfgdeveloper.manresapp.databinding.FragmentProfileBinding;
 import com.angelfgdeveloper.manresapp.helpers.Constants;
 import com.angelfgdeveloper.manresapp.ui.LoginActivity;
@@ -23,16 +25,22 @@ import com.angelfgdeveloper.manresapp.ui.SplashActivity;
 import com.angelfgdeveloper.manresapp.utils.AppConstants;
 import com.angelfgdeveloper.manresapp.utils.SharedPreferencesManager;
 
+import java.util.ArrayList;
+
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
     private FragmentProfileBinding binding;
+
+    private MyPostulationRecyclerViewAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        getData();
 
         binding.buttonLogout.setOnClickListener(v -> logout());
 
@@ -41,6 +49,19 @@ public class ProfileFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void getData() {
+        ArrayList<Postulation> postulations = new ArrayList<>();
+        postulations.add(new Postulation(1, "Operador de producción", "Lala", "Aún no te han asignado pruebas", "No puedes eliminar esta postulaccción ya que la empresa ha depurados a los candidados y has sido un seleccionado."));
+        postulations.add(new Postulation(2, "Agente de ventas/Cambaceo", "Takis - Barcell", "Aún no te han asignado pruebas", ""));
+        postulations.add(new Postulation(3, "Cobrador con motocicleta propia", "Elektra", "Aún no te han asignado pruebas", "No puedes eliminar esta postulaccción ya que la empresa ha depurados a los candidados y has sido un seleccionado."));
+        postulations.add(new Postulation(4, "Agente de ventas/Cambaceo", "Takis - Barcell", "Aún no te han asignado pruebas", ""));
+
+        adapter = new MyPostulationRecyclerViewAdapter(getActivity(), postulations);
+        binding.recyclerViewApplications.setAdapter(adapter);
+        binding.recyclerViewApplications.setLayoutManager(new LinearLayoutManager(getContext()));
+
     }
 
     private void logout() {
