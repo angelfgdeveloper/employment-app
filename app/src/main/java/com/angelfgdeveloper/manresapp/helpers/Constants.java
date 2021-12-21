7c66095d054a040417aa31f20d40d320fea12aa2
Luis Angel <angelfgdeveloper@gmail.com>
2021-12-20 18:55:03 -0600

    Prototipo de mensaje de exito al rellena información

--- app/src/main/java/com/angelfgdeveloper/manresapp/helpers/Constants.java
+++ app/src/main/java/com/angelfgdeveloper/manresapp/helpers/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String IS_LOGIN_USER = "IS_LOGIN_USER";
     public static final String IS_CLOSED_PREVIEW_TIME = "IS_CLOSED_PREVIEW_TIME";
     public static final String IS_REGISTER_USER = "IS_REGISTER_USER";
+    public static final String IS_COMPLETE_TEST_SUCCESS = "IS_COMPLETE_TEST_SUCCESS";
 }
--- app/src/main/java/com/angelfgdeveloper/manresapp/ui/NavigationActivity.java
+++ app/src/main/java/com/angelfgdeveloper/manresapp/ui/NavigationActivity.java
@@ -1,6 +1,7 @@
 package com.angelfgdeveloper.manresapp.ui;
 
 import android.app.ActivityOptions;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import com.angelfgdeveloper.manresapp.ui.exit.ExitFragment;
 import com.angelfgdeveloper.manresapp.ui.profile.EditProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -31,6 +33,7 @@ public class NavigationActivity extends AppCompatActivity implements
     private BottomNavigationView navView;
 
     private boolean isLogin = false;
+    private String isCompleteTestSuccess = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +43,12 @@ public class NavigationActivity extends AppCompatActivity implements
 
     private void init() {
         Bundle args = getIntent().getExtras();
-        if(args != null){
+        if (args != null) {
             isLogin = args.getBoolean(Constants.IS_LOGIN_USER);
+
+            if (args.getString(Constants.IS_COMPLETE_TEST_SUCCESS) != null) {
+                isCompleteTestSuccess = args.getString(Constants.IS_COMPLETE_TEST_SUCCESS);
+            }
         }
 
         Toast.makeText(this, "Acceso por Login: " + isLogin, Toast.LENGTH_SHORT).show();
@@ -58,12 +65,30 @@ public class NavigationActivity extends AppCompatActivity implements
             navView.getMenu().removeItem(R.id.navigation_exit);
         }
 
+        if (isCompleteTestSuccess.equals("Success_Test")) {
+            showCustomDialog();
+        }
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_navigation);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         Bundle bundle = new Bundle();
         bundle.putBoolean(Constants.IS_LOGIN_USER, isLogin);
         navController.navigate(R.id.navigation_home, bundle);
+    }
+
+    private void showCustomDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogCustom);
+        builder.setTitle("Información guardada");
+        builder.setMessage("Los datos fueron guardados exitosamente!");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(NavigationActivity.this, isCompleteTestSuccess, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
